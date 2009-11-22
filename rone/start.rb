0393e5827eca458049d0be5dfefca3283bca37d4
basyura <basyura@gmail.com>
2009-11-22 23:28:54 +0900

    ランキング表示に必要なデータをキャッシュする

--- rone/start.rb
+++ rone/start.rb
@@ -3,49 +3,19 @@ gem 'rack' , '0.9.1'
 require 'sinatra'
 require 'pstore'
 require 'sequel'
-require 'tusers_perfecture'
 
 set :run, true
 
 DB = Sequel.sqlite("../tusers.db")
-class Users < Sequel::Model ; end
+
+require 'tusers_perfecture'
+require 'utils/cache.rb'
+require 'models/user.rb'
 
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
-  def top100
-    rank = nil
-    PStore.new("../data/total_rank.store").transaction(true) {|store|
-      rank = store[:rank]
-    }
-    rank
-  end
-  def users(location , page=1)
-    page = page ? page.to_i - 1 : 0
-    num = 100
-    Users.filter(:location_conv => location).order(:id).limit(num,num*page)
-    #users = []
-    #PStore.new("../data/#{location}.store").transaction(true) {|store|
-    #  users = store[:root]
-    #}
-    #list = []
-    #len = (page * num + num)
-    #len = users.length if len > users.length
-    #for i in (page * num)...len
-    #  list << users[i]
-    #end
-    #puts list[0]
-    #list
-  end
-  def ranking
-    list = []
-    Tusers::PERFECTURE.each_pair{|key , value|
-      list << [key , value , Users.filter(:location_conv => key).count]
-    }
-    list.sort{|a,b| b[2] <=> a[2]}
-  end
 end
-
 
 get '/?' do
   erb :index
